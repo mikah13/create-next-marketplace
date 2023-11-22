a1798bc786db0493bd62ebe1e96aa758c1617df2
mikah13 <anhminhhoang13@gmail.com>
2023-11-21 20:43:52 -0800

    basic product test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,21 +62,21 @@ export default async function Home() {
   );
 }
 
-// async function CrudShowcase() {
-//   const session = await getServerAuthSession();
-//   if (!session?.user) return null;
+async function CrudShowcase() {
+  const session = await getServerAuthSession();
+  if (!session?.user) return null;
 
-//   const latestPost = await api.post.getLatest.query();
+  const allProducts = await api.product.getAll.query();
 
-//   return (
-//     <div className="w-full max-w-xs">
-//       {latestPost ? (
-//         <p className="truncate">Your most recent post: {latestPost.name}</p>
-//       ) : (
-//         <p>You have no posts yet.</p>
-//       )}
+  return (
+    <div className="w-full max-w-xs">
+      {allProducts ? (
+        allProducts.map((product) => <div key={product.id}>{product.name}</div>)
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
 
-//       <CreatePost />
-//     </div>
-//   );
-// }
+      {/* <CreatePost /> */}
+    </div>
+  );
+}
