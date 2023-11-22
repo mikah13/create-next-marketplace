0f38269fdbb780a8f67e8a18955df1e32622164a
mikah13 <anhminhhoang13@gmail.com>
2023-11-21 21:09:53 -0800

    define object type for Product

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -3,18 +3,24 @@ import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 import { products } from "@/server/db/schema";
 
+export const TProductObject = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1),
+  price: z.number().nonnegative(),
+  quantity: z.number().nonnegative(),
+});
+
+
 export const productRouter = createTRPCRouter({
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(TProductObject)
     .mutation(async ({ ctx, input }) => {
       await ctx.db.insert(products).values({
-        name: "Some product",
-        description: "This is some product",
-        price: 3.14,
-        quantity: 1,
-        sellerId: "d88815ee-acc2-4c4d-9c00-e96be0c60af2",
+        ...input,
+        sellerId: ctx.session.user.id,
       });
     }),
+
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.query.products.findMany({
       orderBy: (products, { desc }) => [desc(products.createdAt)],
