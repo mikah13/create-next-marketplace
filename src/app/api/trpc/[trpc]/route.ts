3ca27fbe4986197aa3838801874b847b4d5443bc
mikah13 <anhminhhoang13@gmail.com>
2023-12-01 00:24:03 -0800

    update prettierrc rules

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,23 +1,23 @@
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { type NextRequest } from 'next/server';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+import { type NextRequest } from 'next/server'
 
-import { env } from '@/env.mjs';
-import { appRouter } from '@/server/api/root';
-import { createTRPCContext } from '@/server/api/trpc';
+import { env } from '@/env.mjs'
+import { appRouter } from '@/server/api/root'
+import { createTRPCContext } from '@/server/api/trpc'
 
 const handler = (req: NextRequest) => {
-  return fetchRequestHandler({
-    endpoint: '/api/trpc',
-    req,
-    router: appRouter,
-    createContext: () => createTRPCContext({ req }),
-    onError:
-      env.NODE_ENV === 'development'
-        ? ({ path, error }) => {
-            console.error(`❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`);
-          }
-        : undefined,
-  });
-};
+	return fetchRequestHandler({
+		endpoint: '/api/trpc',
+		req,
+		router: appRouter,
+		createContext: () => createTRPCContext({ req }),
+		onError:
+			env.NODE_ENV === 'development'
+				? ({ path, error }) => {
+						console.error(`❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`)
+				  }
+				: undefined,
+	})
+}
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -1,54 +1,54 @@
-import { createTRPCRouter, protectedProcedure, publicProcedure } from '../trpc';
-import { categories } from '../../db/schema';
-import { z } from 'zod';
-import { response } from '.';
-import { eq } from 'drizzle-orm';
+import { createTRPCRouter, protectedProcedure, publicProcedure } from '../trpc'
+import { categories } from '../../db/schema'
+import { z } from 'zod'
+import { response } from '.'
+import { eq } from 'drizzle-orm'
 
 export const TCategoryObject = z.object({
-  name: z.string().min(1),
-  topicId: z.string().min(1),
-});
+	name: z.string().min(1),
+	topicId: z.string().min(1),
+})
 export const categoryRouter = createTRPCRouter({
-  create: protectedProcedure.input(TCategoryObject).mutation(async ({ ctx, input }) => {
-    const newCategory = await ctx.db.insert(categories).values({
-      ...input,
-    });
-    return newCategory.insertId;
-  }),
+	create: protectedProcedure.input(TCategoryObject).mutation(async ({ ctx, input }) => {
+		const newCategory = await ctx.db.insert(categories).values({
+			...input,
+		})
+		return newCategory.insertId
+	}),
 
-  delete: publicProcedure
-    .input(
-      z.object({
-        id: z.string(),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      const deleteCategory = await ctx.db.delete(categories).where(eq(categories.id, Number(input.id)));
-      return response('record successfully deleted');
-    }),
+	delete: publicProcedure
+		.input(
+			z.object({
+				id: z.string(),
+			})
+		)
+		.mutation(async ({ ctx, input }) => {
+			const deleteCategory = await ctx.db.delete(categories).where(eq(categories.id, Number(input.id)))
+			return response('record successfully deleted')
+		}),
 
-  getAll: publicProcedure.meta({ openapi: { method: 'GET', path: '/category/get-all' } }).query(({ ctx }) => {
-    return ctx.db.query.categories.findMany({
-      orderBy: (categories, { desc }) => [desc(categories.createdAt)],
-    });
-  }),
+	getAll: publicProcedure.meta({ openapi: { method: 'GET', path: '/category/get-all' } }).query(({ ctx }) => {
+		return ctx.db.query.categories.findMany({
+			orderBy: (categories, { desc }) => [desc(categories.createdAt)],
+		})
+	}),
 
-  getCategoryByTopic: publicProcedure
-    .input(
-      z.object({
-        topic: z.string(),
-        limit: z.number().default(12),
-      }),
-    )
-    .query(async ({ ctx, input }) => {
-      const topic = await ctx.db.query.topics.findFirst({
-        where: (topics, { eq }) => eq(topics.name, input.topic),
-      });
-      if (topic) {
-        return ctx.db.query.categories.findMany({
-          where: (categories, { eq }) => eq(categories.topicId, `${topic.id}`),
-          limit: input.limit,
-        });
-      }
-    }),
-});
+	getCategoryByTopic: publicProcedure
+		.input(
+			z.object({
+				topic: z.string(),
+				limit: z.number().default(12),
+			})
+		)
+		.query(async ({ ctx, input }) => {
+			const topic = await ctx.db.query.topics.findFirst({
+				where: (topics, { eq }) => eq(topics.name, input.topic),
+			})
+			if (topic) {
+				return ctx.db.query.categories.findMany({
+					where: (categories, { eq }) => eq(categories.topicId, `${topic.id}`),
+					limit: input.limit,
+				})
+			}
+		}),
+})
