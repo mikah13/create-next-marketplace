b23a8f79168c831fdabcad14e5359635564727a0
mikah13 <anhminhhoang13@gmail.com>
2023-11-30 23:29:41 -0800

    :computer: finish the script, doing test run

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { relations, sql } from "drizzle-orm";
+import { relations, sql } from 'drizzle-orm';
 import {
   bigint,
   float,
@@ -10,8 +10,8 @@ import {
   timestamp,
   varchar,
   boolean,
-} from "drizzle-orm/mysql-core";
-import { type AdapterAccount } from "next-auth/adapters";
+} from 'drizzle-orm/mysql-core';
+import { type AdapterAccount } from 'next-auth/adapters';
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -21,108 +21,103 @@ import { type AdapterAccount } from "next-auth/adapters";
  */
 export const mysqlTable = mysqlTableCreator((name) => `marketplace_${name}`);
 
-export const reviews = mysqlTable("review", {
-  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-  productId: bigint("productId", { mode: "number" }).notNull(),
-  reviewerId: bigint("reviewerId", { mode: "number" }).notNull(),
-  rating: int("rating").notNull(),
-  comment: text("comment"),
-  createdAt: timestamp("created_at")
+export const reviews = mysqlTable('review', {
+  id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
+  productId: bigint('productId', { mode: 'number' }).notNull(),
+  reviewerId: bigint('reviewerId', { mode: 'number' }).notNull(),
+  rating: int('rating').notNull(),
+  comment: text('comment'),
+  createdAt: timestamp('created_at')
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
-  updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow(),
+  updatedAt: timestamp('updatedAt').defaultNow().onUpdateNow(),
 });
 
-export const topics = mysqlTable("topics", {
-  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-  name: varchar("name", { length: 255 }).notNull(),
-  createdAt: timestamp("created_at")
+export const topics = mysqlTable('topics', {
+  id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
+  name: varchar('name', { length: 255 }).notNull(),
+  createdAt: timestamp('created_at')
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
 });
 
-export const categories = mysqlTable("category", {
-  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-  name: varchar("name", { length: 255 }).notNull(),
-  topicId: varchar("topicId", { length: 255 }).notNull(),
-  createdAt: timestamp("created_at")
+export const categories = mysqlTable('category', {
+  id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
+  name: varchar('name', { length: 255 }).notNull(),
+  topicId: varchar('topicId', { length: 255 }).notNull(),
+  createdAt: timestamp('created_at')
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
 });
 
 export const products = mysqlTable(
-  "product",
+  'product',
   {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    sellerId: varchar("sellerId", { length: 255 }).notNull(),
-    categoryId: varchar("categoryId", { length: 255 }).notNull(),
-    name: varchar("name", { length: 255 }).notNull(),
-    description: varchar("description", { length: 16383 }),
-    price: float("price").notNull(),
-    quantity: int("quanity").notNull(),
-    isPublished: boolean("isPublished").notNull(),
-    createdAt: timestamp("created_at")
+    id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
+    sellerId: varchar('sellerId', { length: 255 }).notNull(),
+    categoryId: varchar('categoryId', { length: 255 }).notNull(),
+    name: varchar('name', { length: 255 }).notNull(),
+    description: varchar('description', { length: 16383 }),
+    price: float('price').notNull(),
+    quantity: int('quanity').notNull(),
+    isPublished: boolean('isPublished').notNull(),
+    createdAt: timestamp('created_at')
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow(),
+    updatedAt: timestamp('updatedAt').defaultNow().onUpdateNow(),
   },
   (product) => {
     return {
-      searchCluster: index("search_cluster").on(
-        product.name,
-        product.description,
-      ),
+      searchCluster: index('search_cluster').on(product.name, product.description),
     };
   },
 );
 
-export const productImages = mysqlTable("productImage", {
-  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-  url: varchar("url", { length: 255 }).notNull(),
-  productId: varchar("productId", { length: 255 }).notNull(),
-  createdAt: timestamp("created_at")
+export const productImages = mysqlTable('productImage', {
+  id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
+  url: varchar('url', { length: 255 }).notNull(),
+  productId: varchar('productId', { length: 255 }).notNull(),
+  createdAt: timestamp('created_at')
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
 });
 
 export const users = mysqlTable(
-  "user",
+  'user',
   {
-    id: varchar("id", { length: 255 }).notNull().primaryKey(),
-    name: varchar("name", { length: 255 }),
-    email: varchar("email", { length: 255 }).notNull(),
-    emailVerified: timestamp("emailVerified", {
-      mode: "date",
+    id: varchar('id', { length: 255 }).notNull().primaryKey(),
+    name: varchar('name', { length: 255 }),
+    email: varchar('email', { length: 255 }).notNull(),
+    emailVerified: timestamp('emailVerified', {
+      mode: 'date',
       fsp: 3,
     }).default(sql`CURRENT_TIMESTAMP(3)`),
-    image: varchar("image", { length: 255 }),
+    image: varchar('image', { length: 255 }),
   },
   (user) => ({
-    sellerIdx: index("seller_idx").on(user.id),
+    sellerIdx: index('seller_idx').on(user.id),
   }),
 );
 
 export const accounts = mysqlTable(
-  "account",
+  'account',
   {
-    userId: varchar("userId", { length: 255 }).notNull(),
-    type: varchar("type", { length: 255 })
-      .$type<AdapterAccount["type"]>()
-      .notNull(),
-    provider: varchar("provider", { length: 255 }).notNull(),
-    providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: int("expires_at"),
-    token_type: varchar("token_type", { length: 255 }),
-    scope: varchar("scope", { length: 255 }),
-    id_token: text("id_token"),
-    session_state: varchar("session_state", { length: 255 }),
-    refresh_token_expires_in: int("refresh_token_expires_in"),
+    userId: varchar('userId', { length: 255 }).notNull(),
+    type: varchar('type', { length: 255 }).$type<AdapterAccount['type']>().notNull(),
+    provider: varchar('provider', { length: 255 }).notNull(),
+    providerAccountId: varchar('providerAccountId', { length: 255 }).notNull(),
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
+    expires_at: int('expires_at'),
+    token_type: varchar('token_type', { length: 255 }),
+    scope: varchar('scope', { length: 255 }),
+    id_token: text('id_token'),
+    session_state: varchar('session_state', { length: 255 }),
+    refresh_token_expires_in: int('refresh_token_expires_in'),
   },
   (account) => ({
     compoundKey: primaryKey(account.provider, account.providerAccountId),
-    userIdIdx: index("userId_idx").on(account.userId),
+    userIdIdx: index('userId_idx').on(account.userId),
   }),
 );
 
@@ -172,16 +167,14 @@ export const reviewsRelations = relations(reviews, ({ one, many }) => ({
   }),
 }));
 export const sessions = mysqlTable(
-  "session",
+  'session',
   {
-    sessionToken: varchar("sessionToken", { length: 255 })
-      .notNull()
-      .primaryKey(),
-    userId: varchar("userId", { length: 255 }).notNull(),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
+    sessionToken: varchar('sessionToken', { length: 255 }).notNull().primaryKey(),
+    userId: varchar('userId', { length: 255 }).notNull(),
+    expires: timestamp('expires', { mode: 'date' }).notNull(),
   },
   (session) => ({
-    userIdIdx: index("userId_idx").on(session.userId),
+    userIdIdx: index('userId_idx').on(session.userId),
   }),
 );
 
@@ -190,11 +183,11 @@ export const sessionsRelations = relations(sessions, ({ one }) => ({
 }));
 
 export const verificationTokens = mysqlTable(
-  "verificationToken",
+  'verificationToken',
   {
-    identifier: varchar("identifier", { length: 255 }).notNull(),
-    token: varchar("token", { length: 255 }).notNull(),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
+    identifier: varchar('identifier', { length: 255 }).notNull(),
+    token: varchar('token', { length: 255 }).notNull(),
+    expires: timestamp('expires', { mode: 'date' }).notNull(),
   },
   (vt) => ({
     compoundKey: primaryKey(vt.identifier, vt.token),
--- src/server/db/seed.ts
+++ src/server/db/seed.ts
@@ -1,239 +1,219 @@
-import { eq } from "drizzle-orm";
-import { db } from ".";
-import { categories, products, topics, users } from "./schema";
+import { asc, eq } from 'drizzle-orm';
+import { db } from '.';
+import { categories, products, topics, users } from './schema';
 import { faker } from '@faker-js/faker';
-const topicsData = [
-  "Community",
-  "Housing",
-  "Jobs",
-  "Services",
-  "Discussion Forums",
-  "For Sale",
-  "Gigs",
-];
+const topicsData = ['Community', 'Housing', 'Jobs', 'Services', 'Discussion Forums', 'For Sale', 'Gigs'];
 
 const categoriesData = new Map([
   [
-    "Community",
+    'Community',
     [
-      "activities",
-      "artists",
-      "childcare",
-      "classes",
-      "events",
-      "general",
-      "groups",
-      "local news",
-      "lost+found",
-      "missed connections",
-      "musicians",
-      "pets",
-      "politics",
-      "rants & raves",
-      "rideshare",
-      "volunteers",
+      'activities',
+      'artists',
+      'childcare',
+      'classes',
+      'events',
+      'general',
+      'groups',
+      'local news',
+      'lost+found',
+      'missed connections',
+      'musicians',
+      'pets',
+      'politics',
+      'rants & raves',
+      'rideshare',
+      'volunteers',
     ],
   ],
   [
-    "Housing",
+    'Housing',
     [
-      "apts / housing",
-      "housing swap",
-      "housing wanted",
-      "office / commercial",
-      "parking / storage",
-      "real estate for sale",
-      "rooms / shared",
-      "rooms wanted",
-      "sublets / temporary",
-      "vacation rentals",
+      'apts / housing',
+      'housing swap',
+      'housing wanted',
+      'office / commercial',
+      'parking / storage',
+      'real estate for sale',
+      'rooms / shared',
+      'rooms wanted',
+      'sublets / temporary',
+      'vacation rentals',
     ],
   ],
   [
-    "Services",
+    'Services',
     [
-      "automotive",
-      "beauty",
-      "cell/mobile",
-      "computer",
-      "creative",
-      "cycle",
-      "event",
-      "farm+garden",
-      "financial",
-      "health/well",
-      "household",
-      "labor/move",
-      "legal",
-      "lessons",
-      "marine",
-      "pet",
-      "real estate",
-      "skilled trade",
-      "sm biz ads",
-      "travel/vac",
-      "write/ed/tran",
+      'automotive',
+      'beauty',
+      'cell/mobile',
+      'computer',
+      'creative',
+      'cycle',
+      'event',
+      'farm+garden',
+      'financial',
+      'health/well',
+      'household',
+      'labor/move',
+      'legal',
+      'lessons',
+      'marine',
+      'pet',
+      'real estate',
+      'skilled trade',
+      'sm biz ads',
+      'travel/vac',
+      'write/ed/tran',
     ],
   ],
   [
-    "Discussion Forums",
+    'Discussion Forums',
     [
-      "apple",
-      "arts",
-      "atheist",
-      "autos",
-      "beauty",
-      "bikes",
-      "celebs",
-      "comp",
-      "cosmos",
-      "diet",
-      "divorce",
-      "dying",
-      "eco",
-      "feedbk",
-      "film",
-      "fixit",
-      "food",
-      "frugal",
-      "gaming",
-      "garden",
-      "haiku",
-      "help",
-      "history",
-      "housing",
-      "jobs",
-      "jokes",
-      "legal",
-      "manners",
-      "marriage",
-      "money",
-      "music",
-      "open",
-      "parent",
-      "pets",
-      "philos",
-      "photo",
-      "politics",
-      "psych",
-      "recover",
-      "religion",
-      "rofo",
-      "science",
-      "spirit",
-      "sports",
-      "super",
-      "tax",
-      "travel",
-      "tv",
-      "vegan",
-      "words",
-      "writing",
+      'apple',
+      'arts',
+      'atheist',
+      'autos',
+      'beauty',
+      'bikes',
+      'celebs',
+      'comp',
+      'cosmos',
+      'diet',
+      'divorce',
+      'dying',
+      'eco',
+      'feedbk',
+      'film',
+      'fixit',
+      'food',
+      'frugal',
+      'gaming',
+      'garden',
+      'haiku',
+      'help',
+      'history',
+      'housing',
+      'jobs',
+      'jokes',
+      'legal',
+      'manners',
+      'marriage',
+      'money',
+      'music',
+      'open',
+      'parent',
+      'pets',
+      'philos',
+      'photo',
+      'politics',
+      'psych',
+      'recover',
+      'religion',
+      'rofo',
+      'science',
+      'spirit',
+      'sports',
+      'super',
+      'tax',
+      'travel',
+      'tv',
+      'vegan',
+      'words',
+      'writing',
     ],
   ],
   [
-    "For Sale",
+    'For Sale',
     [
-      "antiques",
-      "appliances",
-      "arts+crafts",
-      "atv/utv/sno",
-      "auto parts",
-      "aviation",
-      "baby+kid",
-      "barter",
-      "beauty+hlth",
-      "bike parts",
-      "bikes",
-      "boat parts",
-      "boats",
-      "books",
-      "business",
-      "cars+trucks",
-      "cds/dvd/vhs",
-      "cell phones",
-      "clothes+acc",
-      "collectibles",
-      "computer parts",
-      "computers",
-      "electronics",
-      "farm+garden",
-      "free",
-      "furniture",
-      "garage sale",
-      "general",
-      "heavy equip",
-      "household",
-      "jewelry",
-      "materials",
-      "motorcycle parts",
-      "motorcycles",
-      "music instr",
-      "photo+video",
-      "rvs+camp",
-      "sporting",
-      "tickets",
-      "tools",
-      "toys+games",
-      "trailers",
-      "video gaming",
-      "wanted",
-      "wheels+tires",
+      'antiques',
+      'appliances',
+      'arts+crafts',
+      'atv/utv/sno',
+      'auto parts',
+      'aviation',
+      'baby+kid',
+      'barter',
+      'beauty+hlth',
+      'bike parts',
+      'bikes',
+      'boat parts',
+      'boats',
+      'books',
+      'business',
+      'cars+trucks',
+      'cds/dvd/vhs',
+      'cell phones',
+      'clothes+acc',
+      'collectibles',
+      'computer parts',
+      'computers',
+      'electronics',
+      'farm+garden',
+      'free',
+      'furniture',
+      'garage sale',
+      'general',
+      'heavy equip',
+      'household',
+      'jewelry',
+      'materials',
+      'motorcycle parts',
+      'motorcycles',
+      'music instr',
+      'photo+video',
+      'rvs+camp',
+      'sporting',
+      'tickets',
+      'tools',
+      'toys+games',
+      'trailers',
+      'video gaming',
+      'wanted',
+      'wheels+tires',
     ],
   ],
   [
-    "Jobs",
+    'Jobs',
     [
-      "accounting+finance",
-      "admin / office",
-      "arch / engineering",
-      "art / media / design",
-      "biotech / science",
-      "business / mgmt",
-      "customer service",
-      "education",
-      "etc / misc",
-      "food / bev / hosp",
-      "general labor",
-      "government",
-      "human resources",
-      "legal / paralegal",
-      "manufacturing",
-      "marketing / pr / ad",
-      "medical / health",
-      "nonprofit sector",
-      "real estate",
-      "retail / wholesale",
-      "sales / biz dev",
-      "salon / spa / fitness",
-      "security",
-      "skilled trade / craft",
-      "software / qa / dba",
-      "systems / network",
-      "technical support",
-      "transport",
-      "tv / film / video",
-      "web / info design",
-      "writing / editing",
+      'accounting+finance',
+      'admin / office',
+      'arch / engineering',
+      'art / media / design',
+      'biotech / science',
+      'business / mgmt',
+      'customer service',
+      'education',
+      'etc / misc',
+      'food / bev / hosp',
+      'general labor',
+      'government',
+      'human resources',
+      'legal / paralegal',
+      'manufacturing',
+      'marketing / pr / ad',
+      'medical / health',
+      'nonprofit sector',
+      'real estate',
+      'retail / wholesale',
+      'sales / biz dev',
+      'salon / spa / fitness',
+      'security',
+      'skilled trade / craft',
+      'software / qa / dba',
+      'systems / network',
+      'technical support',
+      'transport',
+      'tv / film / video',
+      'web / info design',
+      'writing / editing',
     ],
   ],
-  [
-    "Gigs",
-    [
-      "computer",
-      "creative",
-      "crew",
-      "domestic",
-      "event",
-      "labor",
-      "talent",
-      "writing",
-    ],
-  ],
+  ['Gigs', ['computer', 'creative', 'crew', 'domestic', 'event', 'labor', 'talent', 'writing']],
 ]);
 
-console.log("SEEDING START : DB TOPICS ======");
-console.log("REMOVING : DB TOPICS ======");
+console.log('SEEDING START : DB TOPICS ======');
+console.log('REMOVING : DB TOPICS ======');
 // await db.delete(topics);
 // await db.insert(topics).values(
 //   topicsData.map((t) => {
@@ -242,10 +222,10 @@ console.log("REMOVING : DB TOPICS ======");
 //     };
 //   }),
 // );
-console.log("SEEDING END : DB CATEGORIES ======");
+console.log('SEEDING END : DB CATEGORIES ======');
 
-console.log("SEEDING START : DB CATEGORIES ======");
-console.log("REMOVING : DB CATEGORIES ======");
+console.log('SEEDING START : DB CATEGORIES ======');
+console.log('REMOVING : DB CATEGORIES ======');
 // await db.delete(categories);
 
 // for (const [key, value] of categoriesData) {
@@ -262,25 +242,43 @@ console.log("REMOVING : DB CATEGORIES ======");
 //   );
 // }
 
-console.log("SEEDING END : DB CATEGORIES ======");
+console.log('SEEDING END : DB CATEGORIES ======');
 
-console.log("SEEDING START : DB PRODUCTS ======");
+console.log('SEEDING START : DB PRODUCTS ======');
 
 await db.delete(products);
 
-const user = db
-  .select()
-  .from(users)
-  .where(eq(users.email, "anhminhhoang13@gmail.com"))
-  .limit(1);
-  console.log(user)
+const user = await db.select().from(users).where(eq(users.email, 'anhminhhoang13@gmail.com')).limit(1);
+const categoryList = await db.select().from(categories).limit(999).orderBy(asc(categories.id));
+// console.log(user)
+const cIdMin = categoryList[0]?.id;
+const cIdMax = categoryList[categoryList.length - 1]?.id;
 const NUMBER_OF_PRODUCTS = 100;
-// if (user[0]) {
-  // const id = user[0].id;
-// for (let i = 0; i < NUMBER_OF_PRODUCTS; i++) {
-  
-// }
-// }
+if (user[0]) {
+  const id = user[0]?.id;
 
+  const insertProd = new Array(NUMBER_OF_PRODUCTS).fill(1).map((x) => {
+    const name = faker.commerce.productName();
+    const description = faker.commerce.productDescription();
+    const sellerId = id;
+    const price = faker.number.float({ min: 0, max: 999999 });
+    const quantity = faker.number.int({ max: 999 });
+    const categoryId = '' + faker.number.int({ min: cIdMin, max: cIdMax });
 
-console.log("SEEDING END : DB PRODUCTS ======");
+    const isPublished = true;
+
+    return {
+      sellerId,
+      name,
+      description,
+      price,
+      quantity,
+      categoryId,
+      isPublished,
+    };
+  });
+
+  const response = await db.insert(products).values(insertProd);
+}
+
+console.log('SEEDING END : DB PRODUCTS ======');
